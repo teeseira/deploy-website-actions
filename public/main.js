e0ec08288d596085ebdf4d38365b37f635d3d835
teeseira <tidi.matts@gmail.com>
2024-02-05 17:56:40 +0000

    First commit

---
+++ public/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    console.log("Inside ready");
+    $.ajax({
+        type: "GET",
+        url: "target-file.csv",
+        dataType: "text",
+        success: function (data) {
+            processData(data);
+        }
+    });
+});
+
+function processData(csv) {
+    let csvLines = csv.split(/\r\n|\n/);
+    let headers = csvLines[0].split(',');
+    let elGenerate = '<table><tr><th>Person</th><th>Title</th><th>Superpower</th></tr>';
+
+    for (let i = 1; i < csvLines.length; i++) {
+        var data = csvLines[i].split(',');
+        if (data.length == headers.length) {
+            let strOut = "<!-- Table Generate -->\n";
+            strOut = strOut.concat(`<tr><td>${data[0]}</td><td>${data[1]}</td><td>${data[2]}</td></tr>`);
+            elGenerate = elGenerate.concat(strOut);
+        }
+    }
+    
+    let elout = document.getElementById("output");
+    elout.insertAdjacentHTML('beforeend', elGenerate);
+}
